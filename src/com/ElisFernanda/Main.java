c56e9b4151a33ff3ba46987fc941512ef2964e70
elisses <fernandalisses@gmail.com>
2018-09-22 13:55:33 +0200

    Adicionando um objeto no array

--- src/com/ElisFernanda/Main.java
+++ src/com/ElisFernanda/Main.java
@@ -4,22 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double precos[] = new double[5];
-
-        precos[0]= 1000000;
-        precos[1]= 46000;
-        precos[2]= 16000;
-        precos[3]= 46000;
-        precos[4]= 17000;
+        Produto produtos[] = new Produto[5];
+        produtos[0]= new Produto("Lamborghini", 1000000);
+        produtos[1]= new Produto("Jipe", 46000);
+        produtos[2]= new Produto("Brasilia", 16000);
+        produtos[3]= new Produto("Smart", 46000);
+        produtos[4]= new Produto("Fusca", 17000);
 
         int maisBarato = 0;
         for(int atual = 0; atual <= 4; atual++){
-            if(precos[atual] < precos[maisBarato]){
+            if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
                 maisBarato = atual;
             }
         }
         System.out.println("Posição: " + maisBarato);
-        System.out.println("O carro mais barato custa: " + precos[maisBarato]);
+        System.out.println("O carro mais barato custa: " + produtos[maisBarato].getPreco());
 
     }
 }
